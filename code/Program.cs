50ffc40f8108e82838cd8c8650a485c5ec8e9244
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2022-01-31 21:41:06 +0700

    поправил  код

---
+++ code/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,длинна которых <= 3 символам
+
+
+string[]array = {"dog","water","cat","123" , "586" , "11" ,"world"};
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+   
+string[]newArray = array;
+
+for (int j = 0; j < newArray.Length; j++)
+{
+    if (newArray[j].Length <= 3) 
+
+        Console.Write(newArray[j]+" ");
+
+}
+
